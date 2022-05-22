1a8ba31b3ee9e50515d798d36a69916ffd8d07fe
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-05-22 16:20:38 +0200

    Implement MTSS-10 MTSS-12 MTSS-14

--- e-shop-manager/src/main/java/it/unipd/mtss/business/Revenue.java
+++ e-shop-manager/src/main/java/it/unipd/mtss/business/Revenue.java
@@ -38,7 +38,7 @@ public class Revenue implements Bill {
       }
       orderPrice += itemsOrdered.get(i).getPrice();
     }
-    return orderPrice - getSaleIf5Processor(itemsOrdered);
+    return orderPrice - getSaleIf5Processor(itemsOrdered) - freeItemIf10Mice(itemsOrdered) - offerDiscountIfTotalOverThreshold(itemsOrdered) + addFee(itemsOrdered);
   }
 
   double getSaleIf5Processor(List<EItem> itemsOrdered) {
@@ -67,4 +67,75 @@ public class Revenue implements Bill {
 
     return 0;
   }
+
+  //MTSS-10
+  double freeItemIf10Mice(List<EItem> itemsOrdered) {
+    ArrayList<EItem> mice = new ArrayList<>();
+    for (EItem item : itemsOrdered) {
+      if (item.getItem().equals(itemType.Mouse)) {
+        mice.add(item);
+      }
+    }
+
+    if (mice.size() > 10) {
+      Collections.sort(mice, new Comparator<EItem>() {
+        @Override
+        public int compare(EItem z1, EItem z2) {
+          if (z1.getPrice() > z2.getPrice()) {
+            return 1;
+          } else if (z1.getPrice() < z2.getPrice()) {
+            return -1;
+          }
+          return 0;
+        }
+      });
+      EItem cheaper = mice.get(0);
+      return cheaper.getPrice();
+    }
+
+    return 0;
+  }
+
+  //MTSS-12
+  double offerDiscountIfTotalOverThreshold(List<EItem> itemsOrdered) {
+    double total;
+    double threshold;
+    double discount;
+
+    total = 0.0;
+    threshold = 1000.0;
+    discount = 0.0;
+
+    for (EItem item : itemsOrdered) {
+      total += item.getPrice();
+    }
+    if (total > threshold) {
+      discount = total * 0.1;
+    }
+
+    return discount;
+  }
+
+  }
+
+  //MTSS-14
+  double addFee(List<EItem> itemsOrdered) {
+    double total;
+    double threshold;
+    double fee;
+
+    total = 0.0;
+    threshold = 10.0;
+    fee = 2.0;
+
+    for (EItem item : itemsOrdered) {
+      total += item.getPrice();
+    }
+    if (total < threshold) {
+      return fee;
+    }
+
+    return 0.0;
+  }
+
 }
