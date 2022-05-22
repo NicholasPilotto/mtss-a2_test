1fddeccaa9bb8e3f459a3444d7d5b9919bbf407e
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-05-22 16:20:55 +0200

    Implement tests for MTSS-10 MTSS-12 MTSS-14

--- e-shop-manager/src/test/java/it/unipd/mtss/business/RevenueTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/business/RevenueTest.java
@@ -13,7 +13,6 @@ import it.unipd.mtss.model.User;
 
 import java.util.ArrayList;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -28,9 +27,13 @@ public class RevenueTest {
   private Revenue revenue;
   private User user;
 
+  private int freeMouseThreshold;
+
   private ArrayList<EItem> emptyList;
   private ArrayList<EItem> nullList;
   private ArrayList<EItem> itemsList;
+  private ArrayList<EItem> miceList;
+  private ArrayList<EItem> belowThresholdList;
 
   @Before
   public void setUpObject() {
@@ -38,15 +41,24 @@ public class RevenueTest {
     motherboard = new EItem("RoG zy 1080", 1099.99, itemType.Motherboard);
     mouse = new EItem("Asus L3", 9.99, itemType.Mouse);
     processor = new EItem("Intel i5", 49.99, itemType.Processor);
+    freeMouseThreshold = 10; //soglia per ricevere gratis il mouse più economico
+
     emptyList = new ArrayList<EItem>();
     nullList = new ArrayList<EItem>();
     itemsList = new ArrayList<EItem>();
+    miceList = new ArrayList<EItem>();
+    belowThresholdList = new ArrayList<EItem>();
+
     nullList.add(keyboard);
     nullList.add(null);
     itemsList.add(keyboard);
     itemsList.add(motherboard);
     itemsList.add(mouse);
     itemsList.add(processor);
+    for (int i = 0; i < freeMouseThreshold; i++) {
+      miceList.add(mouse);
+    }
+    belowThresholdList.add(mouse);
 
     user = new User("MarioRossi", "Mario", "Rossi");
 
@@ -72,4 +84,51 @@ public class RevenueTest {
   public void getTotalTest() {
     assertEquals(1205.57, revenue.getOrderPrice(itemsList, user), 0.0);
   }
+
+  //MTSS-10
+  @Test
+  public void freeItemIf10MiceTest() {
+      assertEquals(99.90, revenue.getOrderPrice(miceList, user), 0.0);
+  }
+
+  //MTSS-10
+  @Test
+  public void freeItemIfMoreThan10MiceTest() {
+      miceList.add(mouse);
+      assertEquals(99.90, revenue.getOrderPrice(miceList, user), 0.0);
+  }
+
+  //MTSS-10
+  @Test
+  public void freeItemIfMoreThan10MiceTest() {
+      miceList.add(mouse);
+      assertEquals(99.90, revenue.getOrderPrice(miceList, user), 0.0);
+  }
+
+  //MTSS-12
+  @Test
+  public void offerDiscountIfTotalOverThresholdTest() {
+      assertEquals(1085.01, revenue.getOrderPrice(itemsList, user), 0.0); //alzo il delta a 1cent?
+  }
+
+  //MTSS-12
+  //è necessario?
+  @Test
+  public void donotOfferDiscountIfTotalOverThresholdTest() {
+      assertEquals(99.90, revenue.getOrderPrice(miceList, user), 0.0);
+  }
+
+    //MTSS-14
+  @Test
+  public void addFeeTest() {
+      assertEquals(11.99, revenue.getOrderPrice(belowThresholdList, user), 0.0);
+  }
+
+  //MTSS-14
+  //è necessario?
+  @Test
+  public void donotOfferDiscountIfTotalOverThresholdTest() {
+      assertEquals(99.90, revenue.getOrderPrice(miceList, user), 0.0);
+  }
+
 }
