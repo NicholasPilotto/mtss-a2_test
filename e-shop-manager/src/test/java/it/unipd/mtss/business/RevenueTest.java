86eb0bc4fd5761a1ef5701d43ee3784252490659
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-05-22 14:34:28 +0200

    RevenueTest

--- e-shop-manager/src/test/java/it/unipd/mtss/business/RevenueTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/business/RevenueTest.java
@@ -5,6 +5,57 @@
 
 package it.unipd.mtss.business;
 
+import it.unipd.mtss.business.exception.BillException;
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.User;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 class RevenueTest {
-  
+    private EItem keyboard;
+    private EItem motherboard;
+    private EItem mouse;
+    private EItem processor;
+
+    @Before
+    public void setUpObject() {
+        keyboard = new EItem("Logitech x 300", 45.6, itemType.Keyboard);
+        motherboard = new EItem("RoG zy 1080", 1099.99, itemType.Motherboard);
+        mouse = new EItem("Asus L3", 9.99, itemType.Mouse);
+        processor = new EItem("Intel i5", 49.99, itemType.Processor);
+        ArrayList<EItem> emptyList = new ArrayList<EItem>();
+        ArrayList<EItem> nullList = new ArrayList<EItem>();
+        ArrayList<EItem> itemsList = new ArrayList<EItem>();
+        nullList.add(keyboard);
+        nullList.add(null);
+        itemsList.add(keyboard);
+        itemsList.add(motherboard);
+        itemsList.add(mouse);
+        itemsList.add(processor);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getTotalNullTest() {
+        getTotal(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getTotalEmptyArrayListTest() {
+        getTotal(emptyList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getTotalNullArrayListTest() {
+        getTotal(nullList);
+    }
+
+    @Test
+    public void getTotalTest() {
+        assertEquals(1205.57, getTotal(itemsList), 0.0);
+    }
 }
